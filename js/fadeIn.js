389fce6c721f228f0026b5a74dcfb34550fb2133
Derrick Lin <delin@e1z2r8p36.42.us.org>
2018-04-25 10:09:02 -0700

    refeshin

--- js/fadeIn.js
+++
@@ -1,9 +0,0 @@
-shots = $(".shot").fadeTo(0, 0.5);
-
-$('.contain').scroll(function(d,h) {
-    shots.each(function(i) {
-        a = $(this).offset().top + $(this).height();
-        b = $('#port').scrollTop() + $('.contain').height();
-        if (a < b) $(this).fadeTo(500,1);
-    });
-});
