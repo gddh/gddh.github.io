e29b54cf250d5108875cca7e34b9803d2afa7c95
Derrick Lin <delin@e1z3r2p4.42.us.org>
2018-06-17 16:58:16 -0700

    completed notes and prjection section

--- menu.js
+++ menu.js
@@ -10,3 +10,37 @@ function myFunction() {
 $(document).ready(function() {
 	$('body.hidden').fadeIn(500).removeClass('hidden')
 })
+
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function() {
+	var href = $.attr(this, 'href');
+
+	$root.stop().animate({
+		scrollTop: $(href).offset().top - 70
+	}, 500);
+
+	return false
+})
+
+
+// ------------------------------
+// https://twitter.com/mattsince87
+// ------------------------------
+
+// function scrollNav() {
+//   $('.nav a').click(function(){  
+//     //Toggle Class
+//     $(".active").removeClass("active");      
+//     $(this).closest('li').addClass("active");
+//     var theClass = $(this).attr("class");
+//     $('.'+theClass).parent('li').addClass('active');
+//     //Animate
+//     $('html, body').stop().animate({
+//         scrollTop: $( $(this).attr('href') ).offset().top - 160
+//     }, 400);
+//     return false;
+//   });
+//   $('.scrollTop a').scrollTop();
+// }
+// scrollNav();
