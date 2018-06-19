8b3d5d2f32fd3863c7b4cbed79ca5fee1f7e1240
Derrick Lin <delin@e1z3r2p4.42.us.org>
2018-06-18 18:36:27 -0700

    fixed fade in

--- menu.js
+++ menu.js
@@ -25,7 +25,7 @@ $('a[href^="#"]').click(function() {
 
 function fadeIn(){
 	$('.grid-item').each( function(i){
-		var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+		var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 50;
 		var bottom_of_window = $(window).scrollTop() + $(window).height();
 
 		/* If the object is completely visible in the window, fade it it */
